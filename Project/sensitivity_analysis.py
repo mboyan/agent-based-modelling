8ca2f631a21b3c50e9486155696e19d1bb73de4d
Eline van de Lagemaat <elinevdla@gmail.com>
2024-07-10 21:29:22 +0200

    Lorge cleanup

--- Project/sensitivity_analysis.py
+++ Project/sensitivity_analysis.py
@@ -10,13 +10,26 @@ from IPython.display import clear_output
 def run_batches(model, problem, outputs, n_max_timesteps, n_replicates, n_distinct_samples, seed=123):
     """
     Generate parameter samples with SALib and run batches of model replicates.
+    
     Args:
-        problem: SALib problem dictionary
-        outputs: list of output variables to record
-        n_max_timesteps: maximum number of timesteps to run the model for
-        n_replicates: number of replicates per sample
-        n_distinct_samples: number of distinct samples to generate
-        seed: random seed
+    --------
+    problem: dictionary
+        SALib problem dictionary
+    outputs: list 
+        List of output variables to record.
+    n_max_timesteps : int
+        Maximum number of timesteps to run the model for.
+    n_replicates: int
+        Number of replicates per sample.
+    n_distinct_samples : int
+        Number of distinct samples to generate.
+    seed : int
+        Seed number for random number generators.
+        
+    Returns:
+    --------
+    data : pandas.DataFrame
+        Data for batch runs of simulations.
     """
 
     np.random.seed(seed)
@@ -76,11 +89,22 @@ def run_batches(model, problem, outputs, n_max_timesteps, n_replicates, n_distin
 def sobol_analyse(data, problem, outputs, mean_over_last):
     """
     Analyse the collected data with Sobol sensitivity analysis and plot the results.
+    
     Args:
-        data (pd.DataFrame): collected data from the model runs
-        problem (dict): SALib problem dictionary
-        outputs (list of str): list of output variable names to analyse
-        mean_over_last (int): number of timesteps to average over for each replicate
+    -------
+    data : pd.DataFrame
+        Collected data from the model runs.
+    problem : dict
+        SALib problem dictionary.
+    outputs : list of str
+        List of output variable names to analyse.
+    mean_over_last : int
+        Number of timesteps to average over for each replicate.
+        
+    Returns:
+    --------
+    sensitivity_indices: list
+        List of calculated sensitivty indices from Sobol analysis.
     """
 
     max_steps = data['Step'].max() + 1
@@ -105,3 +129,75 @@ def sobol_analyse(data, problem, outputs, mean_over_last):
         sensitivity_indices[output] = Si
     
     return sensitivity_indices
+
+
+def run_ofat_batches(model, problem, defaults, outputs, n_max_timesteps, n_replicates, n_distinct_samples, seed=123):
+    """
+    Generate parameter samples as linear ranges and run batches of model replicates, one parameter at a time.
+    
+    Args:
+    --------
+    problem : dict
+        SALib problem dictionary.
+    defaults : dict
+        Default parameter values.
+    outputs : list of str
+        List of output variables to record
+    n_max_timesteps : int
+        Maximum number of timesteps to run the model for.
+    n_replicates : int
+        Number of replicates per sample.
+    n_distinct_samples : int
+        Number of distinct samples to generate within parameter range.
+    seed : int
+        Seed number for random number generators.
+        
+    Returns:
+    --------
+    data : pd.DataFrame
+        Dataframe with collected OFAT data.
+    """
+
+    np.random.seed(seed)
+    random.seed(seed)
+
+    # Find indices of parameters that should be integers
+    integer_param_names = ['n_init_trees', 'n_init_fungi', 'width', 'height', 'harvest_nbrs']
+    param_types = [int if name in problem['names'] else float for name in integer_param_names]
+
+    collected_data = []
+
+    count = 0
+    for i, param in enumerate(problem['names']):
+
+        samples = np.linspace(problem['bounds'][i][0], problem['bounds'][i][1], n_distinct_samples, dtype=param_types[i])
+
+        for val in samples:
+            variable_parameters = defaults.copy()
+            variable_parameters[param] = val
+
+            run_result = batch.batch_run(model, variable_parameters,
+                                         iterations=n_replicates, max_steps=n_max_timesteps,
+                                         data_collection_period=1)
+
+            # Get relevant keys from the run result
+            run_keys_relevant = [key for key in run_result[0].keys() if key in outputs or key in ['RunId', 'Step']
+                                 or key in problem['names'] or key in defaults.keys()]
+            run_result_relevant = []
+            for step in range(len(run_result)):
+                run_result_step = {key: run_result[step][key] for key in run_keys_relevant}
+                run_result_step['SimId'] = i * n_distinct_samples + step
+                run_result_relevant.append(run_result_step)
+
+            # Store the results in the data frame
+            collected_data.extend(run_result_relevant)
+
+            clear_output()
+
+            count += 1
+            print(f"{count / (len(problem['names']) * samples.shape[0]) * 100:.2f}% done")
+    
+    data = pd.DataFrame(collected_data)
+    data.reset_index(drop=True, inplace=True)
+
+    return data
--- Project/visualisation.py
+++ Project/visualisation.py
@@ -1,34 +1,43 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib as mpl
-import seaborn as sns
+from mesa.experimental import JupyterViz
 import plotly.graph_objs as go
 import plotly.io as pio
 pio.renderers.default = 'notebook'
 
-import numpy as np
-import agent as agt
-
-from mesa.experimental import JupyterViz
 from itertools import combinations
 from matplotlib.cm import ScalarMappable
 from matplotlib.colors import Normalize
 
-'''
-Analysis stuff
-- phase space plot
-- bifurcation analysis
-'''
+"""
+Visualisation functions for data analysis of Forest model.
+"""
 
 def agent_portrayal(agent):
+    """
+    Assigns colors, sizes and transparancies to agents for visualisation on grid.
+    
+    Args:
+    -------
+    agent : Agent
+        Agent to be assigned stylistics features.
+    
+    Returns:
+    style : dict
+        Stylistic features for given agent.
+    """
     size = 5
     color = "black"
     alpha = 1
+    # Fungus agent
     if agent.agent_type == "Fungus":
         size = 4
         color = "tab:blue"
         alpha = agent.energy / 4
-    if agent.agent_type == "Tree":
+    # Tree agent
+    elif agent.agent_type == "Tree":
         size = agent.volume
         if agent.infected:
             color = "tab:olive"
@@ -40,19 +49,36 @@ def agent_portrayal(agent):
 
 def create_jupyter_viz(model, model_params, measures):
     """
-    Function to create a JupyterViz object for the model.
+    Creates a JupyterViz object for the model.
+    
+    Args:
+    --------
+    model: Model
+        Model to be visualized.
+    model_params : dict
+        Dictionary of all parameters needed to initialize model.
+    measures : list
+        List of all metrics to keep track of in visualization.
+        
+    Returns:
+    --------
+    vis : JupyterViz
+        Interactive visualisation object.
     """
-
-    page = JupyterViz(model, model_params, measures, name="FA Model", agent_portrayal=agent_portrayal)
-
-    return page
+    return JupyterViz(model, model_params, measures, name="FA Model", agent_portrayal=agent_portrayal)
 
 
 def plot_property_layer(model, layer_name):
     """
     Function to plot a property layer of the model.
+    
+    Args:
+    --------
+    model: Model
+        Model to be plotted.
+    layer_name : string
+        Name of property layer to be plotted.
     """
-
     plt.imshow(model.grid.properties[layer_name].data, cmap='viridis')
     plt.title(layer_name)
     plt.colorbar()
@@ -65,17 +91,20 @@ def plot_index(s, params):
     of each parameter to the global sensitivity.
 
     Args:
-        s (dict): nested dictionary {'output': {'S#': dict, 'S#_conf': dict}} that holds
-            the values for a set of parameters of all outputs.
-        params (list): the parameters taken from s
+    s : dict 
+        Nested dictionary {'output': {'S#': dict, 'S#_conf': dict}} that holds
+        the values for a set of parameters of all outputs.
+    params : list
+        The parameters taken from s.
     """
-
     # Order of Si
     orders = ['1', '2', 'T']
     order_names = ['First', 'Second', 'Total']
 
+    # Plot all sensitivity indices
     for output in s.keys():
         for i, order in enumerate(orders):
+            # Second order indices
             if order_names[i] == 'Second':
                 p = len(params)
                 params_combo = list(combinations(params, 2))
@@ -87,7 +116,6 @@ def plot_index(s, params):
                 params_combo = params
                 indices = s[output]['S' + order]
                 errors = s[output]['S' + order + '_conf']
-                # plt.figure()
 
             l = len(indices)
 
@@ -102,16 +130,22 @@ def plot_index(s, params):
 
             fig.show()
 
+
 def query_simulation_run(data, sim_id, outputs, problem):
     """
     Retrieve and plot the time series data for a specific simulation run.
+    
     Args:
-        data (pd.DataFrame): collected data from the model runs
-        sim_id (int): the ID of the simulation run to plot
-        outputs (list of str): list of output variable names to plot
-        problem (dict): SALib problem dictionary
+    -------
+    data : pd.DataFrame
+        Collected data from the model runs.
+    sim_id : int
+        The ID of the simulation run to plot.
+    outputs : list of str
+        List of output variable names to plot.
+    problem : dict
+        SALib problem dictionary.
     """
-
     # Get the data for the specific run
     run_data = data[data['SimId'] == sim_id]
     run_data.reset_index(drop=True, inplace=True)
@@ -143,15 +177,22 @@ def query_simulation_run(data, sim_id, outputs, problem):
 def plot_param_space(data, output, param1, param2, mean_over_last=100, ax=None):
     """
     Plot specific outputs in a sampled 2D parameter space.
+    
     Args:
-        data (pd.DataFrame): collected data from the model runs
-        output (str): name of the output of interest
-        param1 (str): name of the first parameter to plot
-        param2 (str): name of the second parameter to plot
-        mean_over_last (int): number of timesteps to average over for each replicate
-        ax (matplotlib.axes.Axes): axes object to plot on
+    -------
+    data : pd.DataFrame
+        Collected data from the model runs.
+    output : string
+        Name of the output of interest.
+    param1 : string
+        Name of the first parameter to plot.
+    param2 : string
+        Name of the second parameter to plot.
+    mean_over_last : ing
+        Number of timesteps to average over for each replicate.
+    ax : matplotlib.axes.Axes
+        Axes object to plot on.
     """
-
     if ax is None:
         fig, ax = plt.subplots()
         fig.set_size_inches(6, 5)
@@ -159,7 +200,8 @@ def plot_param_space(data, output, param1, param2, mean_over_last=100, ax=None):
         fig = ax.get_figure()
 
     max_sims = data['SimId'].max() + 1
-
+    
+    # Set names for parameter labels
     param_name_mapping = {
         'harvest_volume': '$V_H$',
         'harvest_nbrs': '$N_H$',
@@ -167,11 +209,11 @@ def plot_param_space(data, output, param1, param2, mean_over_last=100, ax=None):
         'top_n_sites_percent': '$P_\%$'
     }
 
-
     param_values_1 = []
     param_values_2 = []
     mean_outputs = []
 
+    # Calculate means of  all simulations for chosen parameters
     for sim_id in range(max_sims):
         data_subset = data[data['SimId'] == sim_id]
         n_values = data_subset.shape[0]
@@ -187,10 +229,10 @@ def plot_param_space(data, output, param1, param2, mean_over_last=100, ax=None):
     # Set cmap range
     vmin = min(mean_outputs)
     vmax = max(mean_outputs)
-    cmap = mpl.colormaps['viridis'] #plt.cm.get_cmap('viridis')
+    cmap = mpl.colormaps['viridis'] 
 
+    # Plot data
     ax.scatter(param_values_1, param_values_2, c=mean_outputs, cmap=cmap, vmin=vmin, vmax=vmax, marker='.', s=5)
-
     ax.set_xlabel(param_name_mapping[param1])
     ax.set_ylabel(param_name_mapping[param2])
     ax.set_title(output, fontsize=11)
@@ -207,17 +249,23 @@ def plot_param_space(data, output, param1, param2, mean_over_last=100, ax=None):
 def plot_param_space_array(data, params, outputs):
     """
     Create a series of 2D plots for each pair of input parameters.
+    
     Args:
-        data (pd.DataFrame): collected data from the model runs
-        params (list of str): list of parameter names to plot
-        outputs (list of str): list of output variable names to plot
+    -------
+    data : pd.DataFrame)
+        Collected data from the model runs.
+    params : list of str
+        List of parameters to plot.
+    outputs : list of str
+        List of output variables to plot.
     """
-
+    # Combine all parmeters to plot them against eachother
     param_combos = list(combinations(params, 2))
 
     fig, ax = plt.subplots(len(outputs), len(param_combos))
     fig.set_size_inches(2*len(param_combos), 2*len(outputs))
 
+    # Plot all parameter combinations
     for i, output in enumerate(outputs):
         for j, combo in enumerate(param_combos):
             plot_param_space(data, output, combo[0], combo[1], ax=ax[i, j])
@@ -229,14 +277,21 @@ def plot_param_space_array(data, params, outputs):
 def plot_param_range(data, param, output, param_range, mean_over_last=100):
     """
     Create a plot that shows the output for a specific parameter over a range of values.
+    
     Args:
-        data (pd.DataFrame): collected data from the model runs
-        param (str): name of the parameter to plot
-        outputs (str): name of the output of interest
-        param_range (tuple): (min, max) range of values for the parameter
-        mean_over_last (int): number of timesteps to average over for each replicate
+    -------
+    data : pd.DataFrame)
+        Collected data from the model runs.
+    param : string
+        Name of the parameter to plot.
+    outputs : string
+        Name of the output of interest.
+    param_range : tuple
+        (min, max) range of values for the parameter.
+    mean_over_last : int
+        Number of timesteps to average over for each replicate.
     """
-
+    # Set names for parameter labels
     param_name_mapping = {
         'harvest_volume': '$V_H$',
         'harvest_nbrs': '$N_H$',
@@ -247,51 +302,41 @@ def plot_param_range(data, param, output, param_range, mean_over_last=100):
     fig, ax = plt.subplots()
     fig.set_size_inches(4, 3)
     
+    # Filter data for selected parameters
     data_subset = data[(data[param] >= param_range[0]) & (data[param] <= param_range[1])]
     param_values = data_subset[param].unique()
     mean_outputs = []
 
+    # Calculate means for all simulations of selected parameters
     for param_value in param_values:
         data_subset_param = data_subset[data_subset[param] == param_value]
         n_values = data_subset_param.shape[0]
         mean_output = data_subset_param[output].values[n_values - mean_over_last:].mean()
         mean_outputs.append(mean_output)
 
+    # Plot data
     ax.scatter(param_values, mean_outputs, marker='o')
-
     ax.set_xlabel(param_name_mapping[param])
     ax.set_ylabel(output)
     ax.set_title(f'Average {output} over parameter range', fontsize=11)
     fig.show()
     
-    
-def phase_space_2d(data, output_var='Trees', input_var='harvest_nbrs', input_value=None, start_step=50):
-
-    if not input_value:
-        input_value = sorted(data[input_var].unique())[0]
-
-    subset = data[data[input_var] == input_value]
-    subset = subset.sort_values(by=['Step', 'RunId'])
-
-    # Compute mean and derivative over the 20 runs for each time step
-    mean_output = subset.groupby('Step')[output_var].mean().values
-    derivative_output = np.gradient(mean_output)
-
-    # Consider only the data from the specified start step onwards
-    mean_output = mean_output[start_step:]
-    derivative_output = derivative_output[start_step:]
-
-    # Plotting
-    plt.figure(figsize=(10, 6))
-    plt.plot(mean_output, derivative_output, label=f'Derivative vs {output_var}', color='blue')
-    plt.xlabel(output_var)
-    plt.ylabel(f'Derivative of {output_var}')
-    plt.title(f'{output_var} vs Derivative for {input_var} = {input_value}')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
 
 def phase_space_3d(data, output_var='Trees', input_var='harvest_nbrs', start_step=50):
+    """
+    Creates 3D plot of phase space diagrams while varying one parameter.
+    
+    Args:
+    -------
+    data : pandas.Dataframe
+        Dataframe containing simulation data.
+    output_var : string
+        Metric to plot along z-axis
+    input_var : string
+        Metric to vary along x-axis
+    start_step : int
+        Simulation time step to start the plot from.
+    """
     # Initialize lists to store the results
     input_var_values = sorted(data[input_var].unique())
     output_means = []
