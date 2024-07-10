a87e0d167d01b2ac5a5cbd5d0fd423071b4cc4ea
Eline van de Lagemaat <elinevdla@gmail.com>
2024-07-10 17:44:02 +0200

    agents properly commented

--- Project/agent.py
+++ Project/agent.py
@@ -6,8 +6,16 @@ import random
 def check_non_empty(cell_value):
         """
         Check for available property in a given cell.
+        
         Args:
-            cell_value: Value of the cell.
+        ----------
+        cell_value: float
+            Value of the cell.
+        
+        Returns:
+        ----------
+        bool
+            True if the cell value is greater than 0, False otherwise.
         """
         return cell_value > 0
 
@@ -87,7 +95,7 @@ class Tree(Organism):
         Stochastically remove tree agents based
         on their average lifespan (120 years).
     step()
-
+        Executes a single step of the tree's behavior.
 
     """
 
@@ -101,7 +109,7 @@ class Tree(Organism):
         self.v_max = 350
         self.leaffall = 4
 
-        # Mark tree site
+        # Mark tree site in forest model
         self.model.tree_sites[tuple(self.pos)] = True
 
     def grow(self):
@@ -121,7 +129,7 @@ class Tree(Organism):
         Drops leaves on grid cells and attempts to 
         infect woody substrate on forest floor.
         """
-        # check for substrate
+        # Check for substrate
         substrate_cells = self.model.grid.properties['substrate'].select_cells(check_non_empty)
 
         for cell in substrate_cells:
@@ -151,8 +159,9 @@ class Tree(Organism):
             if count_trees >= harvest_nbrs_threshold:
                 # Include an udefined environmental probability
                 if random.random() < harvest_probability:
+                    # Remove tree if all three thresholds are satisfied
                     self.model.harvest_volume += self.volume
-                    self.model.remove_agent(self)  # Remove the tree
+                    self.model.remove_agent(self) 
 
     def stochastic_removal(self):
         ''' 
@@ -166,28 +175,67 @@ class Tree(Organism):
             coord = self.pos
             self.model.grid.properties['substrate'].data[tuple(coord)] += self.volume*0.1
 
-            # Remove tree
+            # Remove tree agent
             self.model.remove_agent(self)
             
 
     def step(self):
         """
-        Tree development step.
+        Executes a single step of the tree's behavior.
         """
         self.grow()
 
+        # Shed leaves in fall
         if self.infected and self.model.schedule.time % self.leaffall == 0:
             self.shed_leaves()
 
+        # Check if tree can be harvested
         if self.harvest():
             return
         
+        # Stochastically die off
         self.stochastic_removal()
 
 
 class Fungus(Organism):
     """
-    A fungus agent.
+    Represents an individual fungus agent in the forest.
+    
+    Attributes
+    ---------------------------------------------------
+    unique_id : int
+        Unique id for agent.
+    model : Model
+        Forest model fungus agent is placed in.
+    pos : tuple
+        Position of fungus on forest grid.
+    disp : int
+        Dispersal coefficient for sporulation on forest floor.
+    init_energy : int
+        Energy a fungus agent is initialized with.
+        
+    Methods
+    ---------------------------------------------------
+    consume()
+        Consumes substrate and deposit fertility on 
+        lattice cell the fungus occupies.
+        Consumes its own energy if no substrate is present.
+    infect_wood()
+        Attempt to infect woody debris based on dispersal
+        coefficient and number of fungi present.
+    infect_tree()
+        Attempt to infect tree based on dispersal coefficient.
+    sporulate()
+        Reproduce to other forest grid cells if woody
+        debris is present.
+        Attempt to reproduce into trees.
+    die()
+        Remove fungus agent from forest.
+    stochastic_removal()
+        Stochastically kill off fungus agent based on average 
+        lifespan (5 years).
+    step()
+        Executes a single step of the fungus' behavior.
     """
 
     def __init__(self, unique_id, model, pos, disp, init_energy=1):
@@ -198,106 +246,115 @@ class Fungus(Organism):
 
     def consume(self):
         """
-        Consume substrate.
+        Consumes substrate and deposit fertility on lattice cell the fungus occupies.
+        Consumes its own energy if no substrate is present.
         """
         x, y = self.pos
         substrate = self.model.grid.properties['substrate'].data[x, y]
         fertility = self.model.grid.properties['soil_fertility'].data[x, y]
 
-        # eat wood + deposit fertility
+        # Eat wood + deposit fertility
         if substrate > 0:
-            # self.model.grid.properties['substrate'].set_cell((x, y), max(0, substrate - 1))
             self.model.grid.properties['substrate'].set_cell((x, y), substrate - 1)
             self.model.grid.properties['soil_fertility'].set_cell((x, y), fertility + 1)
             self.energy += 1
-        # consume reserve if no wood
+        # Consume reserve if no wood present
         else:
             self.energy -= 1
 
     def infect_wood(self, cell):
         '''
-        Try to infect woody debris.
+        Attempt to infect woody debris based on dispersal coefficient and number of fungi present.
+        
+        Args:
+        ------------
+        cell : tuple
+            Forest grid cell to infect (x,y).
         '''
         dist = self.calc_dist(cell, self.pos)
 
-        # count fungi in cell to scale probability with space competition
+        # Count fungi in cell to scale probability with space competition
         contents = self.model.grid.get_cell_list_contents(cell)
         fun_count = sum(1 for agent in contents if isinstance(agent, Fungus))
 
-        # inoculation probability
+        # Inoculation probability
         prob = 1 / (fun_count + 1) * np.exp(-dist / self.disp)
 
-        # probabilistic infection
+        # Probabilistic infection
         if np.random.random() < prob:
-            # self.model.new_agent(Fungus, (x, y))
             self.model.new_agent(Fungus, cell)
 
     def infect_tree(self, tree):
         '''
-        Try to infect tree.
+        Attempt to infect tree based on dispersal coefficient.
+        
+        Args:
+        -----------
+        tree : Agent
+            Tree in forest to infect.   
         '''
         dist = self.calc_dist(tree.pos, self.pos)
         prob = np.exp(-dist / self.disp)
 
-        # print(prob)
-
-        # probabilistic infection
+        # Probabilistic infection
         if np.random.random() < prob:
-            # print("Tree infected!")
             tree.infected = True
 
     def sporulate(self):
         """
-        Reproduce if enough energy.
+        Reproduce to other forest grid cells if woody debris is present.
+        Attempt to reproduce into trees.
         """
         self.energy -= 4
 
-        # substrate infection
+        # Substrate infection
         substrate_layer = self.model.grid.properties['substrate']
 
-        # select cells that have substrate
+        # Select cells that have substrate
         def has_substrate(cell_value):
             return cell_value > 0
 
         woody_cells = substrate_layer.select_cells(has_substrate)
         for cell in woody_cells:
-            # only infect at different location
+            # Only infect at different location
             if cell[0] != self.pos[0] and cell[1] != self.pos[1]:
                 self.infect_wood(cell)
 
-        # tree infection
+        # Tree infection
         trees = self.model.getall("Tree")
-        # print(trees)
         for tree in trees:
             if not tree.infected:
                 self.infect_tree(tree)
 
     def die(self):
         """
-        Die if no energy or killed through environment.
+        Remove fungus agent from forest.
         """
         self.model.remove_agent(self)
 
 
     def stochastic_removal(self):
-        ''' Stochastic removal of fungi: assuming they live on average for 5 years (=20 timesteps = 20*3 months)
-         and the probability of them dying during 5 years is 0.9 = 1−(prob not dying)^20
-        -> then per timestep the probability of stochastic removal is approx 0.1'''
+        ''' 
+        Stochastically kill off fungus agent based on average lifespan (5 years).
+        '''
         if np.random.random() < 0.1:
            self.model.remove_agent(self)
 
     def step(self):
         """
-        Fungus self-development step.
+       Executes a single step of the fungus' behavior.
         """
+        # Eat substrate
         self.consume()
 
+        # Reproduce once a year or die if no energy left
         if self.energy > 4:
             self.sporulate()
         elif self.energy < 1:
             self.die()
             return
 
+        # Stochastically die off
         self.stochastic_removal()
         
         
